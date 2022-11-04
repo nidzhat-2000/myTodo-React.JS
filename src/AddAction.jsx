34fdee090e0058e8f41f78f9acb14cadeeca03ba
nidzhat-2000 <nidzhat.niyazov@gmail.com>
2022-11-05 02:08:45 +0400

    not finished

--- src/AddAction.jsx
+++ src/AddAction.jsx
@@ -1,47 +1,24 @@
 import React, { useEffect, useState } from 'react';
-import { act } from 'react-dom/test-utils';
 import { BiTrash } from 'react-icons/bi';
 import { FaArrowDown, FaArrowUp } from 'react-icons/fa';
 import { MdOutlineDone } from 'react-icons/md';
-
-const sonUmid = () => {
-  const loadData = JSON.parse(localStorage.getItem('acts'));
-  if (loadData !== null) {
-    return [];
-  }
-  return loadData;
-};
+import { BiEdit } from 'react-icons/bi';
+import ShowMessage from './ShowMessage';
 
 function AddAction() {
   const [newAct, setNewAct] = useState('');
-  // const [acts, setActs] = useState([]);
-  const [acts, setActs] = useState(sonUmid());
+  const [acts, setActs] = useState([]);
   const [up, setUp] = useState(false);
-  const randomID = new Date().getTime().toString().slice(-6);
   const [done, setDone] = useState(false);
-  const [added, setAdded] = useState(false);
-
-  // const messageAlert = () => setAdded(!added);
-  console.log(acts);
-
-  useEffect(() => {
-    const messageAlert = setTimeout(() => {
-      // setAdded(present => !present);
-      // console.log(`${acts.length !== 0 ? 'true' : 'false'}`);
-      console.log('not done');
-    }, 2000);
-    return () => {
-      clearTimeout(messageAlert);
-    };
-  }, [acts.length]);
-
-  // setTimeout(() => {
-  //   messageAlert();
-  // }, 2000);
-
-  const makeSubmit = e => {
-    e.preventDefault();
-  };
+  const [message, setMessage] = useState({
+    show: false,
+    type: '',
+    message: '',
+  });
+  const [isEditing, setIsEditing] = useState(false);
+  const [editID, setEditID] = useState(null);
+  const [editName, setEditName] = useState('');
+  const randomID = new Date().getTime().toString().slice(-6);
 
   const toggle = () => {
     setUp(!up);
@@ -52,42 +29,93 @@ function AddAction() {
     console.log(madeAct.title);
   };
 
-  const addNewAct = () => {
-    let newact = newAct.trim();
-    if (!newact) {
-      alert('Please input value !');
-      return;
+  const makeSubmit = e => {
+    e.preventDefault();
+
+    const addNewAct = () => {
+      let newact = newAct.trim();
+      if (!newact) {
+        alert('Please input value !');
+        return;
+      } else if (newact && isEditing) {
+        setActs(
+          acts.map(act => {
+            // console.log(act.actName);
+            if (act.id === editID) {
+              if (newact === editName) {
+                return showMessage(true, 'deleted', "nothing changed");
+              }
+
+              return { ...act, actName: newAct };
+            } else {
+              return act;
+            }
+          })
+        );
+
+        if (newact === editName) {
+          console.log('same');
+        }
+
+        setEditID(null);
+        setIsEditing(false);
+        showMessage(true, 'added', "item's changed");
+      } else {
+        const newItem = { id: randomID, actName: newact };
+        const newActsArr = up ? [newItem, ...acts] : [...acts, newItem];
+        setActs(newActsArr);
+        showMessage(true, 'added', 'item is added');
+        localStorage.setItem('acts', JSON.stringify(newActsArr));
+      }
+      setNewAct('');
+    };
+    addNewAct();
+  };
+
+  useEffect(() => {
+    const loadData = JSON.parse(localStorage.getItem('acts'));
+    if (loadData == null) {
+      setActs([]);
     }
 
-    const newItem = { id: randomID, title: newact };
-    const newActsArr = up ? [newItem, ...acts] : [...acts, newItem];
-    setActs(newActsArr);
-    setNewAct('');
-    localStorage.setItem('acts', JSON.stringify(newActsArr));
-  };
+    setActs(loadData);
+  }, []);
 
   const deleteAct = id => {
     const removed = acts.filter(act => act.id !== id);
-    console.log(removed);
     setActs(removed);
     localStorage.setItem('acts', JSON.stringify(removed));
+    showMessage(true, 'deleted', 'item deleted');
   };
 
   const clearAll = () => {
     setActs([]);
   };
 
-  useEffect(() => {
-    localStorage.setItem('acts', JSON.stringify(acts));
-  }, [acts]);
+  const showMessage = (show = false, type = '', message = '') => {
+    setMessage({ show, type, message });
+  };
 
   // useEffect(() => {
-  //   const loadData = JSON.parse(localStorage.getItem('acts'));
-  //   if (loadData !== null) {
-  //     setActs([]);
-  //   }
-  //   setActs(loadData);
-  // }, []);
+  //   const removerMessage = setTimeout(() => {
+  //     showMessage();
+  //     // console.log('try');
+  //   }, 2000);
+
+  //   return () => {
+  //     clearTimeout(removerMessage);
+  //   };
+  // }, [acts]);
+
+  const editItem = id => {
+    const editedItem = acts.find(act => act.id === id);
+    // console.log(editedItem);
+    setIsEditing(true);
+    console.log(editedItem.actName);
+    setEditName(editedItem.actName);
+    setEditID(id);
+    setNewAct(editedItem.actName);
+  };
 
   return (
     <>
@@ -103,7 +131,7 @@ function AddAction() {
               value={newAct}
               onChange={e => setNewAct(e.target.value)}
             />
-            <button type="button" className="btn add" onClick={addNewAct}>
+            <button type="submit" className="btn add">
               Add action
             </button>
             <button type="button" className="btn up-down" onClick={toggle}>
@@ -111,26 +139,41 @@ function AddAction() {
             </button>
           </div>
         </form>
-        <div className="animation show">
-          {added ? <span>Act is adding...</span> : 'yes'}
-        </div>
+        {message.show && (
+          <ShowMessage
+            {...message}
+            removerMessage={showMessage}
+            changeAct={acts}
+          />
+        )}
       </div>
 
       {acts.length === 0 ? (
-        <div>
-          <span>Nothing yet</span>
+        <div className="nothing">
+          <span>Nothing yet.. </span>
         </div>
       ) : (
         <div className="actions">
-          <ol>
+          <article>
             {acts.map((eachAct, i) => {
-              const { id, title } = eachAct;
+              const { id, actName } = eachAct;
               return (
-                <li key={i} className={`${done ? 'act done' : 'act'}`}>
-                  {eachAct.title}
+                <article key={i} className="flex">
+                  <p>{actName}</p>
                   <div className="btn-cont">
                     <button onClick={() => makeDone(id)} className="btn done">
                       <MdOutlineDone />
+                    </button>
+
+                    <button
+                      className="btn edit"
+                      onClick={() => {
+                        // console.log(id);
+                        editItem(id);
+                      }}
+                      type="submit"
+                    >
+                      <BiEdit />
                     </button>
 
                     <button
@@ -140,10 +183,10 @@ function AddAction() {
                       <BiTrash />
                     </button>
                   </div>
-                </li>
+                </article>
               );
             })}
-          </ol>
+          </article>
           <button onClick={clearAll} className="btn clear">
             Clear All Items
           </button>
---
+++ src/ShowMessage.jsx
@@ -0,0 +1,22 @@
+import React, { useEffect } from 'react';
+
+function ShowMessage({ removerMessage, type, message, changeAct }) {
+  
+  useEffect(() => {
+    const neededTimeOut = setTimeout(() => {
+      removerMessage();
+    }, 1000);
+
+    return () => {
+      clearTimeout(neededTimeOut);
+    };
+  }, [changeAct]);
+
+  return (
+    <div className={`animation mes-${type}`}>
+      <span>{message}</span>
+    </div>
+  );
+}
+
+export default ShowMessage;
